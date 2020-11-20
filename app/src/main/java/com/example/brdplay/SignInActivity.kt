aa02a6b19d466ff717133778aa83f424e030ba5b
Lavínia Paganini <laviniapaganini@gmail.com>
2020-11-20 11:48:10 -0300

    :sparkles: Update the Sign In process with Firebase :sparkles:

--- app/src/main/java/com/example/brdplay/SignInActivity.kt
+++ app/src/main/java/com/example/brdplay/SignInActivity.kt
@@ -2,37 +2,67 @@ package com.example.brdplay
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.brdplay.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_signin.*
 
-class SignInActivity: AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
         auth = Firebase.auth
+        database = Firebase.database.reference
+        buttonSignIn.setOnClickListener {
+            createAccount(
+                    editTextPersonName.text.toString(),
+                    editTextEmailAddress.text.toString(),
+                    editTextPassword.text.toString()
+            )
+        }
     }
 
     public override fun onStart() {
         super.onStart()
         auth.currentUser?.let {
-            updateUI(it, "batata")
+            updateUI(it)
         }
     }
 
-    private fun updateUI(user: FirebaseUser, name: String) {
-        writeNewUser(user.uid, name, user.email)
-
+    private fun updateUI(user: FirebaseUser?) {
         // Go to MainActivity
-        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
-        finish()
+        if (user != null) {
+            startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+            finish()
+        }
     }
+
+    private fun createAccount(name: String, email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(baseContext, "Created user.",
+                            Toast.LENGTH_SHORT).show()
+                        val user = auth.currentUser
+                        writeNewUser(user!!.uid, name, email)
+                        updateUI(user)
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
 
     private fun writeNewUser(userId: String, name: String, email: String?) {
         val user = User(name, email)
