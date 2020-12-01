16bc8e42e35b2d5d03ac5dc856a95dd9d0a6c504
Lavínia Paganini <laviniapaganini@gmail.com>
2020-11-30 22:24:20 -0300

    :construction: Being able to Sign Out of the app :construction: (#1)

--- app/src/main/java/com/example/brdplay/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/brdplay/ui/profile/ProfileFragment.kt
@@ -1,24 +1,31 @@
 package com.example.brdplay.ui.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.brdplay.R
+import com.example.brdplay.SignInActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
 
     private lateinit var profileViewModel: ProfileViewModel
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        auth = Firebase.auth
         profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
@@ -26,6 +33,18 @@ class ProfileFragment : Fragment() {
 //        profileViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+        val signInButton : Button = root.findViewById(R.id.buttonSignOut)
+        signInButton.setOnClickListener {
+            signOut()
+        }
         return root
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        requireActivity().run{
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+        }
     }
 }
